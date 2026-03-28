97391787fbda59399608ffdbc26a883c7968c5c2
Ty Ingram <ty.l.ingram@email.com>
2026-03-28 14:08:30 -0700

    Convert to GUI app with Tkinter

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import tkinter as tk
+
 def main():
-    print("Hello World")
+    window = tk.Tk()
+    window.title("Hello World App")
+    window.geometry("300x200")
+
+    label = tk.Label(window, text="Hello World", font=("Arial", 12))
+    label.pack(expand=True)
+
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
